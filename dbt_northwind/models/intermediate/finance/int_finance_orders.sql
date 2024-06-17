adcc0b36cab104876ee288b3c44d57f1436bbd5f
amenoarnt <107896726+amenoarnt@users.noreply.github.com>
2024-06-17 15:03:43 -0300

    feat: add intermidiate_finance layer

---
+++ dbt_northwind/models/intermediate/finance/int_finance_orders.sql
@@ -0,0 +1,28 @@
+{{ config(
+    materialized='view'
+    ) 
+}}
+
+with orders_cte as (
+    select
+        order_id,
+        order_date,
+        freight
+    from {{ ref('stg_orders') }}
+),
+order_details_cte as (
+    select
+        order_id,
+        quantity,
+        discount
+    from {{ ref('stg_order_details') }}
+)
+
+select
+    o.order_id,
+    o.order_date,
+    c.quantity,
+    c.discount,
+    o.freight
+from orders_cte o
+join order_details_cte c on o.order_id = c.order_id
